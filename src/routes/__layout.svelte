7114749fddd380e12f38e4eeab81ebdbb1bd44d0
Verner Codoceo <vacodoceo@uc.cl>
2022-07-28 20:29:42 -0400

    Fix logo centering

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,25 +9,14 @@
 <div class="relative min-h-full flex flex-col">
 	<!-- Navbar -->
 	<nav class="flex-shrink-0 bg-primary">
-		<div class="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
+		<div class="max-w-7xl mx-auto px-2 sm:px-4">
 			<div class="relative flex items-center justify-between h-16">
 				<!-- Logo section -->
-				<div class="flex items-center px-2 lg:px-0 xl:w-64">
-					<!-- <div class="flex-shrink-0">
-						<img class="h-8 w-auto" src="logo-mercator.png" alt="Logo Mercator" />
-					</div> -->
-				</div>
-
-				<!-- Search section -->
 				<div class="flex-1 flex justify-center">
 					<div class="flex-shrink-0">
 						<img class="h-8 w-auto" src="logo-mercator.png" alt="Logo Mercator" />
 					</div>
-					<!-- <h2 class="font-semibold text-lg tracking-wide text-white">Pozo de oportunidades</h2> -->
 				</div>
-
-				<!-- Links section -->
-				<div class="hidden lg:block lg:w-80" />
 			</div>
 		</div>
 	</nav>
